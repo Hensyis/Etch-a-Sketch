317c744ca3d44efde218e8866105e765588b5577
Jacques <zhou.jacques27@gmail.com>
2022-08-10 21:15:53 +0200

    Réorganiser le code et ajouter une fonctionnalité
    Nous avons réorganiser le code en créant seulement une fonction qui
    permet de créer les cubes.
    Nous avons ajouter la possibilité à l'utilisateur de choisir le nombre
    de cubes qu'il souhaite avoir sur une ligne.

---
+++ EaS.js
@@ -0,0 +1,40 @@
+const container = document.querySelector("#container");
+
+function creationCube(){
+    let utilisateurNombre = getValue();
+    let tailleCube = parseInt(640/utilisateurNombre) - 2;
+    let nombreLigne = parseInt(800/tailleCube);
+    
+    for(let i = 0; i < nombreLigne; i++){
+        for(let j = 0; j < utilisateurNombre ; j++){
+            const div = document.createElement('div');
+            div.style.width = tailleCube+"px";
+            div.style.height = tailleCube+"px"; 
+            div.style.border = "1px solid pink";
+    
+            container.appendChild(div);
+        }
+     }
+    const elements = document.querySelectorAll('#container > div');
+    elements.forEach(element => element.addEventListener('mouseover', ajouterCouleur));
+}
+
+const button = document.querySelector('button');
+button.addEventListener('click', creationCube);
+
+//const elements = document.querySelectorAll('#container > div');
+//elements.forEach(element => element.addEventListener('mouseover', ajouterCouleur));
+
+function ajouterCouleur(e){
+    this.classList.add('colorier')
+}
+
+const input = document.getElementById('nombreCarre');
+
+function getValue(){
+    let utilisateurNombre = document.getElementById('nombreCarre').value;
+    return utilisateurNombre;
+}
+
+
+
