c0cb93e82fefa657a0271e355af6ac5e7e5c4e73
Jacques <zhou.jacques27@gmail.com>
2022-08-16 20:54:17 +0200

    Corrigé le bug de l'alignement des cubes

--- EaS.js
+++ EaS.js
@@ -2,16 +2,16 @@ const container = document.querySelector("#container");
 
 function creationCube(){
     let utilisateurNombre = getValue();
-    let tailleCube = parseInt(640/utilisateurNombre) - 2;
+    let tailleCube = parseInt(800/utilisateurNombre);
     let nombreLigne = parseInt(800/tailleCube);
-    
+
     for(let i = 0; i < nombreLigne; i++){
         for(let j = 0; j < utilisateurNombre ; j++){
             const div = document.createElement('div');
             div.style.width = tailleCube+"px";
             div.style.height = tailleCube+"px"; 
             div.style.border = "1px solid pink";
-    
+            div.style.boxSizing = "border-box";
             container.appendChild(div);
         }
      }
@@ -35,6 +35,7 @@ function getValue(){
     let utilisateurNombre = document.getElementById('nombreCarre').value;
     return utilisateurNombre;
 }
+
 
 
 
